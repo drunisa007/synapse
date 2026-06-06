7f7e44287a6cd9d3226a9e10eb4c9f1779d57dd3
drunisa007 <newarunkatwal@gmail.com>
2026-06-06 20:13:13 +0630

    feat(flutter): add desktop window chrome

--- apps/synapse_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/synapse_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,9 @@ import firebase_messaging
 import flutter_inappwebview_macos
 import flutter_local_notifications
 import path_provider_foundation
+import screen_retriever
 import shared_preferences_foundation
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DesktopWebviewWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewWindowPlugin"))
@@ -20,5 +22,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
--- apps/synapse_app/windows/flutter/generated_plugins.cmake
+++ apps/synapse_app/windows/flutter/generated_plugins.cmake
@@ -6,6 +6,8 @@ list(APPEND FLUTTER_PLUGIN_LIST
   desktop_webview_window
   firebase_core
   flutter_inappwebview_windows
+  screen_retriever
+  window_manager
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
